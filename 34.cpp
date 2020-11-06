a169e95c6cd04a71f98a3572fa3857a6dbe8a6ea
Bhavani Prasad <bhavaniprasad738@gmail.com>
2020-11-07 01:03:48 +0530

    Create 34.cpp

---
+++ 34.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int minoperations(int arr[],int n)
+{
+   int ans = 0; 
+    for (int i=0,j=n-1; i<=j;) 
+    { 
+        if (arr[i] == arr[j]) 
+        { 
+            i++; 
+            j--; 
+        } 
+        else if (arr[i] > arr[j]) 
+        { 
+            j--; 
+            arr[j] += arr[j+1] ; 
+            ans++; 
+        }
+        else
+        { 
+            i++; 
+            arr[i] += arr[i-1]; 
+            ans++; 
+        } 
+    } 
+  
+    return ans; 
+}
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    cin>>arr[i];
+	    cout<<minoperations(arr,n)<<endl;
+	}
+	return 0;
+}
