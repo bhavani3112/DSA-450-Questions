ad9406e87a2e49523ea042a809bdaaddf200608c
Bhavani Prasad <bhavaniprasad738@gmail.com>
2020-11-13 02:57:24 +0530

    Create 157.cpp

---
+++ 157.cpp
@@ -0,0 +1,20 @@
+long long  multiplyTwoLists (Node* l1, Node* l2)
+{
+    long long mod= 1000000007;
+    long long num1 = 0, num2 = 0;
+    while (l1 || l2)
+    {
+        if(l1)
+        {
+            num1 = (num1*10+ l1->data)%mod;
+            l1 = l1->next;
+        }
+        if(l2)
+        {
+            num2 = (num2*10+ l2->data)%mod;
+            l2 = l2->next;
+        }
+    }
+    return (num1*num2)%mod;
+        
+}
