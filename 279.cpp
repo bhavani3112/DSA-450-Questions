f9f38c3d621ebff3ca642600d948e8f786b5871f
Bhavani Prasad <bhavaniprasad738@gmail.com>
2020-11-14 02:12:28 +0530

    Create 279.cpp

---
+++ 279.cpp
@@ -0,0 +1,12 @@
+void reverse(char *str, int len)
+{
+    stack<int>s;
+    for(int i=0;i<len;i++)
+    s.push(str[i]);
+    int i=0;
+    while(!s.empty())
+    {
+        str[i++]=s.top();
+        s.pop();
+    }
+}
