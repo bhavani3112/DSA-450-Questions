09b3559e2d06b58a849272a1524cfb3872f3bcef
Bhavani Prasad <bhavaniprasad738@gmail.com>
2020-11-27 02:45:40 +0530

    Create 171.cpp

---
+++ 171.cpp
@@ -0,0 +1,28 @@
+vector<int> rightView(Node *root)
+{
+    if(root==NULL)
+    return {};
+   vector<int> res;
+   queue<Node*>q;
+   q.push(root);
+   int i,count;
+   while(!q.empty())
+   {
+       count = q.size();
+       if(count<0)
+       break;
+       i=0;
+       while(i++<count)
+       {
+           Node *temp = q.front();
+           q.pop();
+           if(i==count)
+           res.push_back(temp->data);
+           if(temp->left)
+           q.push(temp->left);
+           if(temp->right)
+           q.push(temp->right);
+       }
+   }
+   return res;
+}
