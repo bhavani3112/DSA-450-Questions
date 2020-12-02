dec38c1d7ffd61dc429f58cc8fd05a14eb2146a7
Bhavani Prasad <bhavaniprasad738@gmail.com>
2020-12-03 01:19:48 +0530

    Update 192.cpp

--- 192.cpp
+++ 192.cpp
@@ -6,8 +6,8 @@ Node* lca(Node* root ,int n1 ,int n2 )
     if(root->data == n1 || root->data == n2)
     return root;
     
-    lnode = LCA(root->left, n1, n2);
-    rnode = LCA(root->right, n1, n2);
+    lnode = lca(root->left, n1, n2);
+    rnode = lca(root->right, n1, n2);
     if(lnode && rnode)
     return root;
 }
